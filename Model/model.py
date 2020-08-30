7d731e05894114289e6de83983d312f09d9ed8ab
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2020-08-30 02:21:39 -0400

    model completed

--- Model/model.py
+++ Model/model.py
@@ -1,6 +1,34 @@
 from keras.models import load_model
+from keras.optimizers import Adam
+from tensorflow import expand_dims
+import cv2
+import numpy as np
 
-model = load_model('saved_model/', compile=False)
-model.summary()
+def define_model():
+    model = load_model('saved_model/', compile=False)
+    model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
 
-model('covid19-1.jpg')
+    return model   
+
+def predict(model):
+    img = cv2.imread('normal-1.jpg' , cv2.IMREAD_COLOR)
+    try:
+        img = cv2.resize(img, (256,256))
+
+    except:
+        print('error')
+
+    img = np.array(img)
+    img = expand_dims(img,0)    
+    list = model.predict(img)
+    #print(type(list))
+
+    return np.argmax(list)
+
+if __name__ == '__main__':
+     model = define_model()
+     index = predict(model)
+
+     predictions = ['covid-19','normal','pnuemonia']   
+     #print(index,type(index))  
+     print(predictions[index])
